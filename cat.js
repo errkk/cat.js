f5d17de7f1ec32844c164d67024eee5134d3d080
Eric <errkkgeorge+github@gmail.com>
2011-06-25 10:53:41 +0100

    animating width so the trail can go on the container

--- cat.js
+++ cat.js
@@ -3,44 +3,55 @@
     var c = d.createElement(t),
 	i = d.createElement(t),
 	s = d.getElementsByTagName(t)[0],
-	sp = 200;
+	sf = 200;
+	st = 10000;
 	
     var up = function()
     {
-	$(this).animate({
-	    'marginTop' : '-=10'
-	}, sp, i.down);
+		$(this).animate({
+		    'marginTop' : '-=10'
+		}, sf, i.down);
     }
-    var down = function(){
-	
-	$(this).animate({
-	    'marginTop' : '+=10'
-	}, sp,i.up );
+    var down = function()
+	{
+		$(this).animate({
+		    'marginTop' : '+=10'
+		}, sf,i.up );
     }
     
     i.up = up;
     i.down = down;
     
-    $(i).addClass('i').css({
-	'background' : 'url(/cat.gif) 100% 0 no-repeat',
-	'height' : '100%',
-	'width' : '100%'
-    });
+    $(i).addClass('i')
+		.css({
+		'background' : 'url(/cat.gif) 100% 0 no-repeat',
+		'height' : '100%',
+		'width' : '100%'
+    	});
 	
     i.up();
 	
-    $(c).addClass('cat').hide().fadeIn(1000)
-	.animate({
-	    'left' : '+=700'
-	}, 30000, 'linear')
-	.css({
-	    'position' : 'absolute',
-	    'left' : '-200px',
-	    'top' : '200px',
-	    'z-index' : '100',
-	    'width' : '250px',
-	    'height' : '150px',
-	    'position' : 'absolute'
-	}).html(i);
+    $(c).addClass('c')
+		.hide()
+		.fadeIn(1000)
+		.animate({
+		    'width' : '+=120%'
+		}, st, 'linear', function(){
+			$(this).fadeOut(function(){
+				$(this).remove();
+			});
+		})
+		.css({
+		    'position' : 'absolute',
+		    'left' : '-200px',
+		    'top' : '200px',
+		    'z-index' : '100',
+		    'width' : '250px',
+		    'height' : '150px',
+		    'position' : 'absolute',
+			'background' : 'url(/trail.gif) 100% 0 repeat-x',
+		})
+		.html(i);
     s.parentNode.insertBefore(c,s);
-    }(document,'div'));
+
+}(document,'div'));
